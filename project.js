c01a2bd28b44847ffda8eacfe701569c6c3904e9
PikachuProgrammer <155122499+PikachuProgrammer@users.noreply.github.com>
2024-08-01 18:43:47 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,13 +1,7 @@
 
-const canvas=document.getElementById("canvas")
-const ctx=canvas.getContext("2d")
+const canvas=document.getElementById("canvas");
+const ctx=canvas.getContext("2d");
 
 
-var Player = function(X,Y){
-    this.X=X
-    this.Y=Y
-    
-};
-var player= new Player(2,2);
-
+ctx.rect(10,10,10,10);
 ctx.stroke
