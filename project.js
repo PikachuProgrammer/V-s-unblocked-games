13e3a7b4e13ea6e33a42f3c48de1f00a745caaee
PikachuProgrammer <155122499+PikachuProgrammer@users.noreply.github.com>
2024-08-02 14:04:02 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,7 +1,7 @@
 
-const canvas=document.getElementById("canvas")
-const ctx=canvas.getContext("2d")
+const canvas=document.getElementById("canvas");
+const ctx=canvas.getContext("2d");
 
 
-ctx.rect(10,10,10,10)
-ctx.stroke
+ctx.rect(10,10,10,10);
+ctx.stroke;
